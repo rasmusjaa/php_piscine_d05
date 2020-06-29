eae8d4eb8582b9e311cda4e2dc5ba0d7481f0ee9
Rasmus Jaakonmaki <rjaakonm@c2r2p2.hive.fi>
2020-06-29 15:40:38 +0300

    changed order of sum and modulo

--- ex17/ex17.sql
+++ ex17/ex17.sql
@@ -1,2 +1,2 @@
-SELECT count(*) AS `nb_susc`, FLOOR(AVG(`price`)) AS `av_susc`, SUM(`duration_sub` % 42) AS `ft`
+SELECT count(*) AS `nb_susc`, FLOOR(AVG(`price`)) AS `av_susc`, SUM(`duration_sub`) % 42 AS `ft`
 FROM `subscription`;
